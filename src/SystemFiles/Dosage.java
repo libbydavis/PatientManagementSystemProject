8e7545f1e613e354829e5108676e95341a135981
rajrathod619 <rajrathod619@gmail.com>
2021-03-22 17:49:14 +1300

    added dosage attributes

--- src/SystemFiles/Dosage.java
+++ src/SystemFiles/Dosage.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package assignment1;
+package SystemFiles;
 
 /**
  *
  * @author Raj
  */
 public class Dosage {
+	double amount;
+	String howOften;
     
 }
